eb136519fd976c1ca14496c633f4cbbfac5c891c
OleksandrS <oleksandr.salivon@kanini.com>
2021-12-14 17:57:27 +0200

    Add loading progress to login and register pages

--- app/src/main/java/com/example/booking/fragments/SplashScreenFragment.kt
+++ app/src/main/java/com/example/booking/fragments/SplashScreenFragment.kt
@@ -1,12 +1,15 @@
 package com.example.booking.fragments
 
+import android.app.ProgressDialog
 import android.os.Bundle
 import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import android.widget.Toast
+import androidx.core.widget.ContentLoadingProgressBar
 import androidx.navigation.fragment.findNavController
 import com.example.booking.R
 import java.util.*
@@ -14,7 +17,7 @@ import kotlin.concurrent.schedule
 
 
 class SplashScreenFragment : Fragment() {
-
+        private val loading : ProgressBar? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -22,7 +25,7 @@ class SplashScreenFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_splash_screen, container, false)
        //timer to switch fragments
-        Timer("Test", false).schedule(1000) {
+        Timer("Test", false).schedule(2000) {
             requireActivity().runOnUiThread {
                 findNavController().navigate(R.id.action_splashScreenFragment_to_loginFragment)
             }
