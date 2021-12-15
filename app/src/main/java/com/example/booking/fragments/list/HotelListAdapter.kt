8bba75876189cf706ede2cc0d1acf6ca39207054
OleksandrS <oleksandr.salivon@kanini.com>
2021-12-15 21:44:26 +0200

    Add some style to hotel list, create reservation page

--- app/src/main/java/com/example/booking/fragments/list/HotelListAdapter.kt
+++ app/src/main/java/com/example/booking/fragments/list/HotelListAdapter.kt
@@ -1,10 +1,14 @@
 package com.example.booking.fragments.list
 
 import android.util.Log
+import android.content.Context
+import android.content.res.Resources
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.text.HtmlCompat
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
+import com.example.booking.R
 import com.example.booking.data.Hotel
 import com.example.booking.databinding.HotelRowCustomBinding
 import com.squareup.picasso.Picasso
@@ -25,11 +29,11 @@ class HotelListAdapter(private var hotelList: List<Hotel>) :
         val currentItem = hotelList[position]
         with(holder) {
             with(currentItem) {
-                Log.d("tre", "test")
+                val formattedPricePerNight = "Rooms left:<b> $roomsLeft <b>"
                 binding.hotelName.text = name
                 Picasso.get().load(url).into( binding.hotelPhoto)
-                binding.pricePerNight.text = pricePerNight.toString()
-                binding.roomsLeft.text = roomsLeft.toString()
+                binding.pricePerNight.text = "$pricePerNight $ per night"
+                binding.roomsLeft.text = HtmlCompat.fromHtml("Rooms left:<b> $roomsLeft <b>", HtmlCompat.FROM_HTML_MODE_LEGACY)
 
                 binding.hotelRowCustom.setOnClickListener { view ->
                     val action = HotelsListFragmentDirections.actionHotelsListFragmentToSpecificHotelFragment(currentItem)
@@ -46,36 +50,3 @@ class HotelListAdapter(private var hotelList: List<Hotel>) :
     }
     inner class MyViewHolder(val binding: HotelRowCustomBinding) : RecyclerView.ViewHolder(binding.root)
 }
-//
-//override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListAdapter.MyViewHolder {
-//    val binding = RowCustomBinding
-//        .inflate(LayoutInflater.from(parent.context), parent, false)
-//    return MyViewHolder(binding)
-//}
-//
-//override fun getItemCount() = userList.size
-//
-//
-//override fun onBindViewHolder(holder: ListAdapter.MyViewHolder, position: Int) {
-//    val currentItem = userList[position]
-//    with(holder) {
-//        with(currentItem) {
-//            binding.textViewId.text = id.toString()
-//            binding.textViewFirstName.text = firstName
-//            binding.textViewLastName.text = lastName
-//            binding.textViewAge.text = age.toString()
-//
-//            binding.rowLayout.setOnClickListener { view ->
-//                val action = ListFragmentDirections.actionListFragmentToUpdateFragment(currentItem)
-//                Navigation.findNavController(view).navigate(action)
-//            }
-//        }
-//
-//    }
-//}
-//
-//fun setData(user: List<User>) {
-//    this.userList = user
-//    notifyDataSetChanged()
-//}
-//inner class MyViewHolder(val binding: RowCustomBinding) : RecyclerView.ViewHolder(binding.root)
