c366d917e87194cc5e6b8197a58e8bcf9f267186
arestus <arestus@gmail.com>
2021-12-15 15:39:58 +0200

    Add drawer-menu and routes

--- app/src/main/java/com/example/booking/MainActivity.kt
+++ app/src/main/java/com/example/booking/MainActivity.kt
@@ -4,13 +4,20 @@ package com.example.booking
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.booking.data.Hotel
 import com.example.booking.data.LocalUser
 import com.example.booking.data.LoginHistory
 
 import com.example.booking.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -23,8 +30,13 @@ import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
 
         val myApplication = application as BookingApp
         val httpApiService = myApplication.httpApiService
@@ -44,7 +56,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 //      setContentView(R.layout.activity_main)
 
-        setupActionBarWithNavController(findNavController(R.id.fragmentContainerView))
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navController = findNavController(R.id.fragmentContainerView)
+        binding.navigationView.setupWithNavController(navController)
+
+
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
 
 
     }
@@ -53,6 +71,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.fragmentContainerView)
-        return navController.navigateUp() || super.onSupportNavigateUp()
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
