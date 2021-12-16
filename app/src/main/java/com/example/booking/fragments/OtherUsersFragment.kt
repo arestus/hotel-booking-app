52788b6c42a2ce1d500f0b9fabd4309bbbba452b
arestus <arestus@gmail.com>
2021-12-16 17:45:05 +0200

    Add 3 main back buttons

--- app/src/main/java/com/example/booking/fragments/OtherUsersFragment.kt
+++ app/src/main/java/com/example/booking/fragments/OtherUsersFragment.kt
@@ -5,16 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.booking.R
+import com.example.booking.databinding.FragmentOtherUsersBinding
+import com.example.booking.databinding.FragmentProfileBinding
 
 class OtherUsersFragment : Fragment() {
+    private var _binding: FragmentOtherUsersBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _binding = FragmentOtherUsersBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_other_users, container, false)
+        binding.otherUsersBack.setOnClickListener{
+            findNavController().navigate(R.id.action_otherUsersFragment_to_hotelsListFragment)
+        }
+//        return inflater.inflate(R.layout.fragment_other_users, container, false)
+        return binding.root
     }
 
 
