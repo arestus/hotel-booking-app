fc1589b12238d4d05b347c35655d6233c5c5354e
Edem Nedipov <edenpolovets@gmail.com>
2021-12-20 12:59:25 +0300

    added member since

--- app/src/main/java/com/example/booking/fragments/list/loginHistory/LoginHistoryFragment.kt
+++ app/src/main/java/com/example/booking/fragments/list/loginHistory/LoginHistoryFragment.kt
@@ -1,5 +1,6 @@
 package com.example.booking.fragments.list.loginHistory
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,14 +10,18 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.booking.R
+import com.example.booking.SessionManager
 import com.example.booking.databinding.FragmentLoginHistoryBinding
 import com.example.booking.viewmodels.LoginHistoryViewModel
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class LoginHistoryFragment : Fragment() {
     private lateinit var mUserViewModel: LoginHistoryViewModel
     private var _binding: FragmentLoginHistoryBinding? = null
     private val binding get() = _binding!!
+    private lateinit var session: SharedPreferences
 
 
 
@@ -24,6 +29,7 @@ class LoginHistoryFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        session = SessionManager(requireActivity())
 
         _binding = FragmentLoginHistoryBinding.inflate(inflater, container, false)
         var adapter = LoginHistoryListAdapter(emptyList())
@@ -40,8 +46,19 @@ class LoginHistoryFragment : Fragment() {
             findNavController().navigate(R.id.action_loginHistoryFragment_to_profileFragment)
         }
         binding.loginHistoryTitle.text = "Login history"
+        var memberFrom = getDateTime((session as SessionManager).getTime())
+        binding.title.text = "Member since: + $memberFrom "
         setHasOptionsMenu(true)
         return binding.root
+    }
+    private fun getDateTime(s: String): String? {
+        try {
+            val sdf = SimpleDateFormat("dd MMMM yyyy")
+            val netDate = Date(s.toLong())
+            return sdf.format(netDate)
+        } catch (e: Exception) {
+            return e.toString()
+        }
     }
 
 
